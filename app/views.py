f43471608eb86b3a5b6121febc700e2d4b4d75c9
SweenEy <ronnie.alonso@gmail.com>
2015-01-31 11:14:32 -0800

    Separate HTML layout

--- app/views.py
+++ app/views.py
@@ -1,6 +1,23 @@
 from flask import render_template
 from app import *
 
-@application.route('/')
-def index():
+@application.route('/signin', methods = ['GET'])
+def Signin():
+    return render_template('signin.html')
+
+@application.route('/full', methods = ['GET'])
+def Full():
+    return render_template('full.html')
+
+@application.route('/index', methods = ['GET'])
+def Index():
     return render_template('index.html')
+
+@application.route('/messages', methods = ['GET'])
+def Messages():
+    return render_template('message.html')
+
+@application.route('/events', methods = ['GET'])
+def Events():
+    return render_template('events.html')
+
